cc145932a6643108c51d1df18ee8a7dd7d7de54d
Paulo Andrade <source.compu@gmail.com>
2015-11-20 10:57:07 -0600

    Se agrego la dependencia gulp-connect para correr un servidor web con live reload activado, se configuro recargar automaticamente el navegador al momento de guardar cambios en el archivo es6.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 // Importamos las dependencias
 var gulp = require('gulp'),
+    connect = require('gulp-connect'),
 	browserify = require('browserify'),
 	babelify = require('babelify'),
 	buffer = require('vinyl-buffer'),
 	source = require('vinyl-source-stream');
+
+// Tarea para crear el servidor
+gulp.task('server', function (){
+    connect.server({
+        root: './app',
+        port: 3000,
+        livereload: true
+    });
+});
+
+// Tarea para recargar el html automaticamente
+gulp.task('html', function (){
+    return gulp.src('./app/index.html')
+    .pipe(connect.reload());
+});
+
+// Tarea para recargar el html automaticamente
+gulp.task('scripts', function (){
+    return gulp.src('./app/es6/*.js')
+    .pipe(connect.reload());
+});
 
 // Tarea para compilar de ES6 a ES5
 gulp.task('build', function (){
@@ -27,8 +49,10 @@ gulp.task('build', function (){
 
 // Tarea para estar a la escucha de cambios
 gulp.task('watch', function (){
+    gulp.watch(['./app/index.html'], ['html']);
+    gulp.watch(['./app/es6/*.js'], ['scripts']);
 	gulp.watch(['./app/es6/es6.js'], ['build']);
 });
 
 // Tarea por defecto
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['server', 'build', 'watch']);
