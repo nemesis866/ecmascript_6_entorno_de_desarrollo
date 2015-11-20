1ef84f758f81c1a5f63caf662b3151b520871730
Paulo Andrade <source.compu@gmail.com>
2015-11-20 12:56:20 -0600

    Agregamos la dependencia gulp-connect para crear un servidor con livereload activado, este servidor funciona en el modo developer.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,39 @@
 // Importamos las dependencias
 import gulp from 'gulp';
+import connect from 'gulp-connect';
 import browserify from 'browserify';
 import babelify from 'babelify';
 import buffer from 'vinyl-buffer';
 import source from 'vinyl-source-stream';
 
+// Tarea para correr servidor web
+gulp.task('server', () => {
+    return connect.server({
+        root: './app',
+        port: 3000,
+        livereload: true
+    });
+});
+
+// Tarea para recargar el navegador (.html)
+gulp.task('html', () => {
+    return gulp.src('./app/index.html')
+    .pipe(connect.reload());
+});
+
+// Tarea para recargar el navegador (.js)
+gulp.task('scripts', () => {
+    return gulp.src('./app/es6/*.js')
+    .pipe(connect.reload());
+});
+
 // Tarea para compilar de ES6 a ES5
 gulp.task('build', () => {
 	const b = browserify({
     	entries: ['./app/es6/es6.js'],
-    	transform: [babelify]
+    	transform: [babelify.configure({
+            presets: ['es2015']
+        })]
     })
 
     return b.bundle()
@@ -24,8 +48,10 @@ gulp.task('build', () => {
 
 // Tarea para estar a la escucha de cambios
 gulp.task('watch', () => {
+    gulp.watch(['./app/index.html'], ['html']);
+    gulp.watch(['./app/es6/*.js'], ['scripts']);
 	gulp.watch(['./app/es6/es6.js'], ['build']);
 });
 
 // Tarea por defecto
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['server', 'build', 'watch']);
